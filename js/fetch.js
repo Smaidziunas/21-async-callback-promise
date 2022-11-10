23c2d8160ba7e49b2ceb4fde085afb4b8ca5dbf4
Smaidziunas <sdamarcius@gmail.com>
2022-11-10 08:04:43 +0200

    6 uzd

--- js/fetch.js
+++ js/fetch.js
@@ -1,6 +1,8 @@
 "use strict";
 console.log("fetch.js");
 
+const usersListEl = document.getElementById("users");
+
 // norim is interneto parsisiusti duomenis ir su jais sugeneruoti postus
 // fetch(url)
 // .then((response) => response.json())
@@ -27,12 +29,70 @@ console.log("fetch.js");
 
 const url = "https://jsonplaceholder.typicode.com/users";
 
+// /*
 fetch(url)
   .then((response) => response.json()) //.json() = JSON.parse();
   .then((dataInJsFormat) => {
     // gavau duomenis
     console.log("dataInJsFormat ===", dataInJsFormat);
+    generateUsersList(dataInJsFormat);
+  })
+  .catch((err) => console.warn("klaida gaunant users", err));
+// */
+
+// 1. is gautu duomenu atvaizduoti sarasa su vardais ir tel. numeriais
+
+/* sukuriant variable
+
+let newObj = [];
+
+fetch(url)
+  .then((response) => response.json())
+  .then((listTelName) => {
+    // console.log("listTelName ===", listTelName);
+    listTelName.map((card) => {
+      newObj.push({ name: card.name, phone: card.phone });
+    });
+  });
+
+console.log(newObj);
+*/
+
+/* sukuriant atskira funkcija ir ja idedant i fetch
+fetch(url)
+  .then((response) => response.json()) //.json() = JSON.parse();
+  .then((dataInJsFormat) => {
+    // gavau duomenis
+    console.log("dataInJsFormat ===", dataInJsFormat);
+
+    // iskelti saraso generavimo logika i atskira funkcija generateUsersList()
+    dataInJsFormat.forEach((uObj) => {
+      // uObj === {id: 1, name: 'Leanne Graham', username: 'Bret', email: 'Sincere@april.biz', address: {…}, …}
+      const oneEl = makeOneLi(uObj.name, uObj.phone);
+      usersListEl.append(oneEl);
+    });
   })
   .catch((err) => console.warn("klaida gaunant users", err));
 
-// is gautu duomenu atvaizduoti sarasa su vardais ir tel. numeriais
+function makeOneLi(name, tel) {
+  const liEl = document.createElement("li");
+  liEl.textContent = `Name: ${name}, tel: ${tel}`;
+  return liEl;
+}
+*/
+
+// 2. iskelti saraso generavimo logika i atskira funkcija generateUsersList()
+
+function makeOneLi(name, tel) {
+  const liEl = document.createElement("li");
+  liEl.textContent = `Name: ${name}, tel: ${tel}`;
+  return liEl;
+}
+
+function generateUsersList(arr) {
+  arr.forEach((uObj) => {
+    // uObj === {id: 1, name: 'Leanne Graham', username: 'Bret', email: 'Sincere@april.biz', address: {…}, …}
+    const oneEl = makeOneLi(uObj.name, uObj.phone);
+    usersListEl.append(oneEl);
+  });
+}
---
+++ js/practice.js
@@ -0,0 +1,52 @@
+"use strict";
+console.log("practice.js");
+
+const url = "https://jsonplaceholder.typicode.com/posts";
+const url2 = " https://dummyjson.com/posts";
+
+fetch(url)
+  .then((response) => response.json())
+  .then((duomenysJavascriptFormatu) => {
+    // console.log('duomenysJavascriptFormatu ===', duomenysJavascriptFormatu);
+    const first10posts = duomenysJavascriptFormatu.slice(0, 10);
+    console.log("first10posts ===", first10posts);
+    generateCards(first10posts, "users");
+  })
+  .catch((err) => console.warn("klaida gaunant posts", err));
+
+/*
+  <article class="card">
+      <h3>title</h3>
+      <p>text</p>
+      <p><i>id</i></p>
+    </article>
+  */
+function generateCards(arr, elId) {
+  const dest = document.getElementById(elId);
+  if (!dest) throw "blogas id";
+
+  const htmlElsArr = arr.map((p) => {
+    const articleEl = document.createElement("article");
+    articleEl.className = "card";
+    articleEl.innerHTML = `
+    <h3>${p.title}</h3>
+    <p>${p.body}</p>
+    <p><i>${p.id}</i></p>
+    `;
+    return articleEl;
+  });
+  console.log("htmlElsArr ===", htmlElsArr);
+  htmlElsArr.forEach((el) => dest.append(el));
+}
+
+// 6. parsisiusti postus
+
+fetch(url2)
+  .then((respo) => respo.json())
+  .then((data) => {
+    console.log("data ===", data);
+    console.log("data.posts ===", data.posts);
+  })
+  .catch((err) => {
+    console.warn("klaida gaunant postus", err);
+  });
